e2d63027db054752afe8eb90318fbdad08965cec
Guilherme Moura Ignacio <guilherme.ignacio@fcamara.com.br>
2018-09-29 02:52:29 -0300

    segunda parte

--- game.js
+++ game.js
@@ -42,19 +42,63 @@ playGame.prototype = {
      this.launchPanel.body.immovable = true;
 
      var ballSize = game.width * gameOptions.ballSize;
-     this.ball = game.add.sprite(game.width / 2, game.height - this.launchPanel.height, "ball");
+     this.ball = game.add.sprite(game.width / 2, game.height - this.launchPanel.height - ballSize/2, "ball");
      this.ball.width = ballSize;
      this.ball.height = ballSize;
      this.ball.anchor.set(0.5);
      game.physics.enable(this.ball, Phaser.Physics.ARCADE);
 
-     this.ball.body.colliderWorldBounds = true;
+     this.ball.body.collideWorldBounds = true;
      this.ball.body.bounce.set(1);
 
      this.trajectory = game.add.sprite(this.ball.x, this.ball.y, "trajectory");
      this.trajectory.anchor.set(0.5, 1);
      this.trajectory.visible = false;
+
+     game.input.onDown.add(this.aimBall, this);
+     game.input.onUp.add(this.shootBall, this);
+     game.input.addMoveCallback(this.adjustBall, this);
+
+     this.aiming = false;
+     this.shooting = false;
  },
 
- update: function () {},
+ aimBall: function(e){
+     if(!this.shooting){
+         this.aiming = true;
+     }
+ },
+
+ adjustBall: function(e){
+    if(this.aiming){
+        var distY = e.position.y - e.positionDown.y;
+        if(distY > 10){
+            this.trajectory.position.set(this.ball.x, this.ball.y);
+            this.trajectory.visible = true;
+            this.direction = Phaser.Math.angleBetween(e.position.x, e.position.y, e.positionDown.x, e.positionDown.y);
+            this.trajectory.angle = Phaser.Math.radToDeg(this.direction) + 90;
+        } else {
+            this.trajectory.visible = false;
+        }
+    }
+ },
+
+ shootBall: function() {
+     if(this.trajectory.visible){
+         var angleOfFire = Phaser.Math.degToRad(this.trajectory.angle - 90);
+         this.ball.body.velocity.set(gameOptions.ballSpeed * Math.cos(angleOfFire), gameOptions.ballSpeed  * Math.sin(angleOfFire));
+         this.shooting = true;
+     }
+     this.aiming = false;
+     this.trajectory.visible = false;
+ },
+
+ update: function () {
+     if(this.shooting){
+         game.physics.arcade.collide(this.ball, this.launchPanel, function(){
+            this.ball.body.velocity.set(0);
+            this.shooting = false;
+         },null, this);
+     }
+ },
 }
